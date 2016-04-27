4e6ddd7ae608c9da2aec0e232da8104197b80a4e
Chope <yoonhg2002@gmail.com>
2016-04-27 22:49:39 +0900

    new project

---
+++ CPSelectionManager/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  CPSelectionManager
+//
+//  Created by Chope on 2016. 4. 27..
+//  Copyright (c) 2016 Chope. All rights reserved.
+//
+
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+
+
+}
