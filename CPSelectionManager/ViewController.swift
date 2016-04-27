746e54a455827063f590ec51e24fd0d762323627
yoonhg84 <yoonhg2002@gmail.com>
2016-04-28 00:53:25 +0900

    샘플 추가

--- CPSelectionManager/ViewController.swift
+++ CPSelectionManager/ViewController.swift
@@ -11,20 +11,49 @@ import UIKit
 
 
 class ViewController: UIViewController {
+    @IBOutlet var collectionOfItemButtons: [UIButton] = [UIButton]()
+    @IBOutlet var collectionOfMenuButtons: [UIButton] = [UIButton]()
+    @IBOutlet weak var countLabel: UILabel? = nil
+    var itemSelectionManager = CPSelectionManager()
+    var menuSelectionManager = CPSelectionManager()
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
 
 
     override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+        super.viewDidLoad()
+
+        self.itemSelectionManager.selectionType = .Single
+        self.itemSelectionManager.changedSelection = { [weak self] (manager: CPSelectionManager) in
+            self?.countLabel?.text = "count : \(manager.countOfSelection())"
+        }
+
+        self.collectionOfItemButtons.flatMap { (button: UIButton) in
+            self.itemSelectionManager.addControl(button)
+        }
+
+        self.collectionOfMenuButtons.flatMap { (button: UIButton) in
+            self.menuSelectionManager.addControl(button)
+        }
+        self.menuSelectionManager.selectionType = .Single
+        self.menuSelectionManager.selectAll()
     }
 
-
-    override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+    @IBAction func single() {
+        self.itemSelectionManager.selectionType = .Single
     }
 
+    @IBAction func multiple() {
+        self.itemSelectionManager.selectionType = .Multiple
+    }
 
+    @IBAction func selectAll() {
+        self.itemSelectionManager.selectAll()
+    }
 
-
+    @IBAction func selectNone() {
+        self.itemSelectionManager.unselectAll()
+    }
 }
