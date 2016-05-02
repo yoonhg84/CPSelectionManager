f4fb3c8e5dfb740ff8ca8072c41edb7a4dba109d
yoonhg84 <yoonhg2002@gmail.com>
2016-05-02 14:43:22 +0900

    add CPSelectionType.LimitedMultiple

--- Source/CPSelectionManager.swift
+++ Source/CPSelectionManager.swift
@@ -8,21 +8,20 @@ import UIKit
 public enum CPSelectionType {
     case Single
     case Multiple
+    case LimitedMultiple(Int)
+}
+
+public enum CPSelectionErrorType: ErrorType {
+    case WrongSelectionType
 }
 
 public class CPSelectionManager {
     public var selectionType = CPSelectionType.Single {
         didSet {
-            if self.selectionType == .Single {
-                if let firstSelectedControl: UIControl = self.selectedControls().first {
-                    self.unselectAll()
-                    firstSelectedControl.selected = true
-                }
-            }
+            self.unselectAll()
         }
     }
     public var changedSelection: ((CPSelectionManager) -> ())? = nil
-
     internal var controls = [UIControl]()
 
     public func addControl(control: UIControl) -> CPSelectionManager {
@@ -36,7 +35,7 @@ public class CPSelectionManager {
         return self
     }
 
-    public func selectAll() {
+    public func selectAll() throws {
         switch self.selectionType {
         case .Multiple:
             if self.countOfSelection() != self.controls.count {
@@ -45,13 +44,8 @@ public class CPSelectionManager {
                 }
                 self.changedSelection?(self);
             }
-        case .Single:
-            if self.countOfSelection() == 0 {
-                if let firstControl = self.controls.first {
-                    firstControl.selected = true
-                    self.changedSelection?(self);
-                }
-            }
+        default:
+            throw CPSelectionErrorType.WrongSelectionType
         }
     }
 
@@ -71,11 +65,21 @@ public class CPSelectionManager {
     }
 
     @objc func touchUpInsideAtControl(control: UIControl) {
-        if self.selectionType == .Single {
+        switch self.selectionType {
+        case .Single:
             self.unselectAll()
+            control.selected = !control.selected
+        case .Multiple:
+            control.selected = !control.selected
+        case .LimitedMultiple(let maxCount):
+            if control.selected {
+                control.selected = false
+            } else if self.countOfSelection() < maxCount {
+                control.selected = !control.selected
+            } else {
+                return
+            }
         }
-
-        control.selected = !control.selected
 
         self.changedSelection?(self);
     }
